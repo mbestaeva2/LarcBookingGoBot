bd2422d5a96731dc3339bb919a76d957eaa67d17
mbestaeva2 <mziabestaeva58@gmail.com>
2025-08-08 19:17:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def on_route_selected(call):
     animals = data.get('animals', 0)
 
     price = calculate_price(adults, children, animals, route)
-   bot.send_message(chat_id, f"✅ Стоимость поездки по маршруту {route}:\n💵 {price}₽")
+    bot.send_message(chat_id, f"✅ Стоимость поездки по маршруту {route}:\n💵 {price}₽")
 
 print("Бот запущен...")
 bot.polling(none_stop=True)
